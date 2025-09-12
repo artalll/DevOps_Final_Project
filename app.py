eb48ae972cb7fbe2d128e62b8873be065c7464ba
artalll <153456021+artalll@users.noreply.github.com>
2025-09-12 14:14:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 """This is the main module of QuakeWatch Flask app."""
 
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route('/')
 def hello():
+    """Return a simple hello message."""
     return 'Hello, World!'
- """Return a simple hello message."""   
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
