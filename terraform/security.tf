57cf278f20733ba3d41376b6e84df2614409a0f0
artalll <153456021+artalll@users.noreply.github.com>
2025-10-25 22:17:00 +0300

    Create security.tf

---
+++ terraform/security.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "k3s_sg" {
+  name        = "k3s-sg"
+  description = "Allow SSH, HTTP, HTTPS, and Kubernetes"
+  vpc_id      = aws_vpc.quakewatch_vpc.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Kubernetes API"
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "k3s-sg" }
+}
